fefe2fa38ce613e84c8cd1502aadfa3b29adc84e
Manish Bharti <manish.bharti@onsumaye.com>
2022-08-19 17:40:04 +0530

    update question name

--- frontend/src/screens/QuestionsList.js
+++ frontend/src/screens/QuestionsList.js
@@ -60,7 +60,7 @@ const QuestionsList = ({match}) => {
                     <thead style={{backgroundColor:'#555555',color:'#ffffff'}}>
                         <tr>
                         <th><h5 style={{color:'#ffffff'}}>Sl. No.</h5></th>
-                        <th><h5 style={{color:'#ffffff'}}>Questions ({`${numDoneQuestion}/${questionsList.length}`})
+                        <th><h5 style={{color:'#ffffff'}}>Questions1 ({`${numDoneQuestion}/${questionsList.length}`})
                         </h5></th>
                         <th><h5 style={{color:'#ffffff'}}>Answer</h5></th>
                         <th><h5 style={{color:'#ffffff'}}>Status</h5></th>
